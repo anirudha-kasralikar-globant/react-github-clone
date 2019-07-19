37a3c396bdee800b6c3f78ca6f4a0be0d9419bb8
Anirudha Kasralikar <anirudha.kasralikar@globant.com>
2019-07-19 20:09:16 +0530

    rearranged packages as group

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,7 +3,7 @@ import './style/main.less';
 
 class Welcome extends React.Component {
   render() {
-    return <h1 className={'header'}>Minimal React Webpack Babel Setup.</h1>;
+    return <h1 className={'header'}>Minimal React Webpack Babel Setup.....</h1>;
   }
 }
 
