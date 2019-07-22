4e2338c77ec14cdd731a393441178423e20c2fb9
Anirudha Kasralikar <anirudha.kasralikar@globant.com>
2019-07-22 15:01:35 +0530

    First commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -31,13 +31,11 @@ class App extends Component<*> {
               overflowY: 'auto',
             }}
           >
-            <switch>
               <Route exact path={ROUTES.LANDING} component={LandingPage} />
               <Route path={ROUTES.SIGN_IN} component={SignInPage} />
               <Route path={ROUTES.HOME} component={HomePage} />
               <Route path={ROUTES.ACCOUNT} component={AccountPage} />
               <Route path={ROUTES.ADMIN} component={AdminPage} />
-            </switch>
           </main>
           <footer role="contentinfo">
             <Menu
