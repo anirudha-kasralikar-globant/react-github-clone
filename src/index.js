f8acb6a8808795736cc9626c5385ecca7566f3dd
Anirudha Kasralikar <anirudha.kasralikar@globant.com>
2019-08-02 13:15:59 +0530

    Redux Thunk CRUD Ajax Posts

--- src/index.js
+++ src/index.js
@@ -2,39 +2,26 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import axe from 'react-axe';
+import { Provider } from 'react-redux';
+import store from './store';
 import './style/main.less';
-import axe from 'react-axe';
-// import { Provider } from 'react-redux'
-// import { createStore, combineReducers } from 'redux';
-
-import App from './component/App';
+import App from './container/App';
 
 declare var module: any;
 
-if (process.env.NODE_ENV !== 'production') {
-  // eslint-disable-next-line no-console
-  console.log('Looks like we are in development mode!');
-}
-
-// const store = createStore(combineReducers({ hello: helloReducer });
-
 const mountNode = document.getElementById('app');
-
-// const wrapApp = (AppComponent, reduxStore) => (
-// <Provider store={reduxStore}>
-//   <AppComponent />
-// </Provider>
-// );
-
 if (mountNode !== null) {
   if (process.env.NODE_ENV !== 'production') {
-    axe(React, ReactDOM, 1000);
-    // ReactDOM.render(wrapApp(<App />, store), mountNode);
-    ReactDOM.render(<App />, mountNode);
-  } else {
-    // ReactDOM.render(wrapApp(<App />, store), mountNode);
-    ReactDOM.render(<App />, mountNode);
+    // axe(React, ReactDOM, 1000);
   }
+
+  const getProvider = () => (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  ReactDOM.render(getProvider(), mountNode);
 }
 
 module.hot.accept();
